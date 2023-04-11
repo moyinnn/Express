4c8c1b9851da486ed49668fabc801ee8ad3fb7a6
moyinnn <moyinoluwaoso18@gmail.com>
2023-04-11 15:23:45 +0100

    express

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 
-console.log("starting app")
+const express = require('express');
+const { link } = require('fs');
+const app = express();
+app.use(express.static('css'));
+app.use(checkWorkingHours);
 
+app.set('view engine', 'pug');
 
-const express = require("express")
+app.get('/', (req, res) => {
+res.render('home');
+});
 
-const path = require("path")
+app.get('/contact', (req, res) => {
+res.render('contact');
+});
 
-const app = express()
+app.get('/services', (req, res) => {
+res.render('services');
+});
 
+app.listen(3000, () => {
+console.log('Server started on port 3000');
+});
+function checkWorkingHours(req, res, next) {
+const now = new Date();
+const dayOfWeek = now.getDay();
+const hourOfDay = now.getHours();
 
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "pug")
-
-
-
-
-app.get('/', function(req, res){
-    res.render('index', {
-        title: "The best cohort",
-        
-    })
-})
-
+if (dayOfWeek >= 1 && dayOfWeek <= 5 && hourOfDay >= 9 && hourOfDay < 17) {
+    
+    next();
+} else {
+    
+    res.status(503).send('Sorry, the service is only available during working hours (Monday to Friday, from 9 to 17).');
+}
+}
 
 app.get('/about', function(req, res){
     res.render('about',)
 })
-
 
 app.listen(3001, function() {
     console.log('server started on port 3001....')
